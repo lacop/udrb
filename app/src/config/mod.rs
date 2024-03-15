3ef2730edafdcf7ea753de3e4d3ae5bf623e7ba6
Laco Papay <lacop@lacop.net>
2024-03-15 19:42:09 +0100

    Switch to YAML config file, and remove secrets from it

--- app/src/config/mod.rs
+++ app/src/config/mod.rs
@@ -1,19 +1,21 @@
-// extern crate toml;
 use std::str::FromStr;
 
-// #[derive(Debug, Clone)]
-// pub struct DomainConfig {
-//     pub id: String,
-//     pub host_regex: regex::Regex,
-//     pub login_page: Option<String>,
-//     pub login_script: Option<String>,
-//     pub render_script: Option<String>,
-// }
+#[derive(Debug, serde::Deserialize)]
+pub struct DomainConfig {
+    pub name: String,
+    #[serde(with = "serde_regex")]
+    pub host: regex::Regex,
+    pub login_page: Option<String>,
+    // TODO: Wrap in SecretString to hide from debug.
+    pub login_script: Option<String>,
+    pub render_script: Option<String>,
+}
 
 #[derive(Debug)]
 pub struct SlackConfig {
     // If empty, requests are not authenticated.
     // TODO: Consider crashing if empty in production build...
+    // TODO: Wrap in SecretString to hide from debug.
     pub secret: Option<String>,
     pub max_age_seconds: Option<i64>,
 }
@@ -24,7 +26,7 @@ pub struct Config {
     pub output_dir: std::path::PathBuf,
     pub chrome_address: String,
     pub slack: SlackConfig,
-    //     pub domains: Vec<DomainConfig>,
+    pub domains: Vec<DomainConfig>,
 }
 
 // Helper to include the variable name in the error message.
@@ -45,48 +47,16 @@ impl Config {
                 .map(|x| x.parse::<i64>().unwrap()),
         };
 
-        let config_path = get_env_var("UDRB_DOMAIN_CONFIG")?;
-        //         let config = std::fs::read_to_string(config_path)?;
-        //         let config: toml::Value = config.parse::<toml::Value>()?;
-
-        //         let mut domains = Vec::new();
-        //         for (id, table) in config.get("domain").unwrap().as_table().unwrap() {
-        //             let host_regex = regex::RegexBuilder::new(table.get("host").unwrap().as_str().unwrap())
-        //                 .case_insensitive(true)
-        //                 .build()
-        //                 .unwrap();
-        //             domains.push(DomainConfig {
-        //                 id: id.to_string(),
-        //                 host_regex,
-        //                 login_page: table
-        //                     .get("login_page")
-        //                     .map(|x| x.as_str().unwrap().to_string()),
-        //                 login_script: table
-        //                     .get("login_script")
-        //                     .map(|x| x.as_str().unwrap().to_string()),
-        //                 render_script: table
-        //                     .get("render_script")
-        //                     .map(|x| x.as_str().unwrap().to_string()),
-        //             });
-        //         }
-        //         let slack = config.get("slack").unwrap();
+        let domain_config_path = get_env_var("UDRB_DOMAIN_CONFIG")?;
+        let domain_config = std::fs::read_to_string(domain_config_path)?;
+        let domains: Vec<DomainConfig> = serde_yaml::from_str(&domain_config)?;
 
         Ok(Config {
             hostname,
             output_dir: std::path::PathBuf::from_str(&output_dir)?,
             chrome_address,
             slack,
-            //             domains,
+            domains,
         })
     }
-
-    //     pub fn get(&self) -> Config {
-    //         // TODO avoid this pointless copy...
-    //         self.0.lock().unwrap().clone()
-    //     }
-
-    //     pub fn get_slack(&self) -> SlackConfig {
-    //         // TODO avoid this pointless copy...
-    //         self.0.lock().unwrap().slack.clone()
-    //     }
 }
